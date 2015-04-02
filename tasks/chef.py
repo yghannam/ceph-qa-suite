76dee22d7488d3c6fcc377aa51a383b117a41bc8
Yazen Ghannam <yazen.ghannam@linaro.org>
2015-04-02 12:35:10 -0400

    Don't use ceph-qa-chef.

--- tasks/chef.py
+++ tasks/chef.py
@@ -12,24 +12,24 @@ def task(ctx, config):
     """
     Run chef-solo on all nodes.
     """
-    log.info('Running chef-solo...')
-
-    run.wait(
-        ctx.cluster.run(
-            args=[
-                'wget',
-#                '-q',
-                '-O-',
-#                'https://raw.github.com/ceph/ceph-qa-chef/master/solo/solo-from-scratch',
-                'http://ceph.com/git/?p=ceph-qa-chef.git;a=blob_plain;f=solo/solo-from-scratch;hb=HEAD',
-                run.Raw('|'),
-                'sh',
-                '-x',
-                ],
-            wait=False,
-            )
-        )
-
-    log.info('Reconnecting after ceph-qa-chef run')
-    misc.reconnect(ctx, 10)     #Reconnect for ulimit and other ceph-qa-chef changes
-
+#    log.info('Running chef-solo...')
+#
+#    run.wait(
+#        ctx.cluster.run(
+#            args=[
+#                'wget',
+##                '-q',
+#                '-O-',
+##                'https://raw.github.com/ceph/ceph-qa-chef/master/solo/solo-from-scratch',
+#                'http://ceph.com/git/?p=ceph-qa-chef.git;a=blob_plain;f=solo/solo-from-scratch;hb=HEAD',
+#                run.Raw('|'),
+#                'sh',
+#                '-x',
+#                ],
+#            wait=False,
+#            )
+#        )
+#
+#    log.info('Reconnecting after ceph-qa-chef run')
+#    misc.reconnect(ctx, 10)     #Reconnect for ulimit and other ceph-qa-chef changes
+#
